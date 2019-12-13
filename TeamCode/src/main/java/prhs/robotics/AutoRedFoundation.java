fea0c62907f0550a3991abcd774ef74cd1872c43
Max Beck <maxwell.beck21@d155.org>
2019-12-13 17:08:02 -0600

    Start working on foundation-side autonomous
    Moves the robot, but not the flippers. Also will not be able
    to move the foundation into the build zone yet, but we are short
    on time and I would rather have something than nothing.

---
+++ TeamCode/src/main/java/prhs/robotics/AutoRedFoundation.java
@@ -0,0 +1,34 @@
+package prhs.robotics;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import java.util.LinkedList;
+
+import prhs.robotics.autonomous.Command;
+import prhs.robotics.autonomous.Controller;
+import prhs.robotics.autonomous.commands.Direction;
+import prhs.robotics.autonomous.commands.Move;
+import prhs.robotics.autonomous.commands.Wait;
+
+@Autonomous(name="Red Team Foundation Side")
+public class AutoRedFoundation extends Controller {
+    private LinkedList<Command> command_queue;
+
+    @Override
+    public void init() {
+        this.command_queue = new LinkedList<>();
+
+        this.command_queue.add(new Move(Direction.BACKWARDS, 35.0));
+        this.command_queue.add(new Wait(0.5));
+        this.command_queue.add(new Move(Direction.FORWARDS, 24.0));
+        this.command_queue.add(new Wait(0.5));
+        this.command_queue.add(new Move(Direction.LEFT, 48.0));
+
+        super.init();
+    }
+
+    @Override
+    public Command nextCommand() {
+        return this.command_queue.pollFirst();
+    }
+}
