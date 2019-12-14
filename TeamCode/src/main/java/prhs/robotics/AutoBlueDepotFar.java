75958e831b2e9d0d7b37258562dfc83f30e4701a
Max Beck <maxwell.beck21@d155.org>
2019-12-14 10:32:50 -0600

    Add near/far variants of depot side autonomous routines
    Allows for greater flexibility with teammates.
    
    Near = near wall
    
    Far = near neutral bridge

--- TeamCode/src/main/java/prhs/robotics/AutoBlueDepotFar.java
+++ TeamCode/src/main/java/prhs/robotics/AutoBlueDepotFar.java
@@ -10,17 +10,17 @@ import prhs.robotics.autonomous.commands.Direction;
 import prhs.robotics.autonomous.commands.Move;
 import prhs.robotics.autonomous.commands.Wait;
 
-@Autonomous(name="Red Team Depot Side")
-public class AutoRedDepot extends Controller {
+@Autonomous(name="Blue Team Depot Side Far")
+public class AutoBlueDepotFar extends Controller {
     private LinkedList<Command> command_queue;
 
     @Override
     public void init() {
         this.command_queue = new LinkedList<>();
 
-        this.command_queue.add(new Move(Direction.FORWARDS, 12.0));
+        this.command_queue.add(new Move(Direction.FORWARDS, 25.0));
         this.command_queue.add(new Wait(0.5));
-        this.command_queue.add(new Move(Direction.RIGHT, 36.0));
+        this.command_queue.add(new Move(Direction.LEFT, 36.0));
 
         super.init();
     }
---
+++ TeamCode/src/main/java/prhs/robotics/AutoRedDepotNear.java
@@ -0,0 +1,32 @@
+package prhs.robotics;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import java.util.LinkedList;
+
+import prhs.robotics.autonomous.Command;
+import prhs.robotics.autonomous.Controller;
+import prhs.robotics.autonomous.commands.Direction;
+import prhs.robotics.autonomous.commands.Move;
+import prhs.robotics.autonomous.commands.Wait;
+
+@Autonomous(name="Red Team Depot Side Near")
+public class AutoRedDepotNear extends Controller {
+    private LinkedList<Command> command_queue;
+
+    @Override
+    public void init() {
+        this.command_queue = new LinkedList<>();
+
+        this.command_queue.add(new Move(Direction.FORWARDS, 3.0));
+        this.command_queue.add(new Wait(0.5));
+        this.command_queue.add(new Move(Direction.RIGHT, 36.0));
+
+        super.init();
+    }
+
+    @Override
+    public Command nextCommand() {
+        return this.command_queue.pollFirst();
+    }
+}
